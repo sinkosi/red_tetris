d57a23aaf052d5a597cbdd9e78090c7acaefc00d
mmamalek <mmamalek@student.wethinkcode.co.za>
2020-09-30 11:53:23 +0200

    I forgot what I changed here

--- frontend/src/components/GameCanvas.jsx
+++ frontend/src/components/GameCanvas.jsx
@@ -1,45 +1,8 @@
 import React, { useEffect, useRef } from "react"
 import { config } from "../misc/canvasConfig"
 import CanvasGrid from "../misc/canvasGrid"
-import Piece, { newPiece } from "../misc/Piece"
+
 //import Swipeable from 'react-swipeable'
-
-function controls(key, piece, grid, context) {
-	if (key === "ArrowUp") piece.rotate()
-	if (key === "ArrowLeft") piece.moveLeft()
-	if (key === "ArrowRight") piece.moveRight()
-	if (key === "ArrowDown") piece.moveDown()
-	if (key === "l") piece.lock()
-	if (key === "p") grid.penalty(piece)
-	if (key === "n") newPiece(context, grid)
-	/*if (key === onSwipedUp) piece.rotate()
-	if (key === onSwipedLeft) piece.moveLeft()
-	PropTypes.func,
-		onSwipedRight: PropTypes.func,
-		onSwipedDown: PropTypes.func,
-		onSwipedLeft: PropTypes.func,onSwip)*/
-}
-
-//function mobile_controls(onSwiped, piec)
-
-// function newPiece(context, grid) {
-// 	if (!context || !grid) return null
-
-// 	let pieceNum = Math.floor(Math.random() * pieces.length)
-// 	let pieceVariant = Math.floor(Math.random() * pieces[pieceNum][0].length)
-// 	console.log(pieceNum, pieceVariant)
-// 	let newPiece = new Piece(context, grid, pieces[pieceNum][0], pieces[pieceNum][1])
-
-// 	newPiece.active = pieceVariant
-// 	let [y, x] = pieceStartPosition(pieceNum, pieceVariant)
-// 	newPiece.x = x
-// 	newPiece.y = y
-// 	newPiece.draw()
-// 	if (newPiece.isCollition(x, y, pieceVariant)) return null
-// 	return newPiece
-// }
-
-// const GameCanvas = (props) => {
 
 const GameCanvas = ({
 	grid,
