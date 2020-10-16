00fd65b4fb006957491b475b86961d4b079e980d
Sibonelo Nkosi <sinkosi@student.wethinkcode.co.za>
2020-10-16 15:58:37 +0200

    I broke it, fixed it by commenting out everything

--- src/client/containers/app.js
+++ src/client/containers/app.js
@@ -1,23 +1,25 @@
 import React from 'react'
 
 
-import CssBaseline from "@material-ui/core/CssBaseline"
-
+//import CssBaseline from "@material-ui/core/CssBaseline"
+/*
 import Header from "../components/Header"
 import Game from "../components/Game"
 
 import "./App.css"
 // import Footer from "./components/Footer"
-
+*/
 function App(props) {
 	return (
-		<>
+		<div>
+			Keep Testing bro!
+			{/*}
 			<CssBaseline>
-				 <Header /> 
+				<Header /> 
 				<Game />
-				{/* <Footer /> */}
-			</CssBaseline>
-		</>
+				<Footer />
+			</CssBaseline>*/}
+		</div>
 	)
 }
 
