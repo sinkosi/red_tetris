86a8095a0286e229a001218f8560497b8d755b25
Sibonelo Nkosi <sinkosi@student.wethinkcode.co.za>
2020-10-19 11:20:14 +0200

    Full merge

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import App from "./App";
+import App from "./app";
 
 ReactDom.render(
   <App message={"An amazing tetris in its way. stay put for more"} />,
