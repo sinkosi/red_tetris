673eddb5a9185ce29964bce66bd5d969676b86bb
Sibonelo Nkosi <sinkosi@student.wethinkcode.co.za>
2020-10-20 08:37:54 +0200

    Pre-merge push

--- src/client/App.js
+++ src/client/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+//import React, { useState } from "react";
+import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 import Header from "./components/Header";
